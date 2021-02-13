e2c9bf481cf74625a807c604bc7da2fc823eff56
santosneto <mn.neco@gmail.com>
2021-02-13 06:37:17 -0300

    update

--- R/Codes.R
+++ R/Codes.R
@@ -1,188 +1,9 @@
-#' Probability Density Function
-#'
-#'
-#'
-#'@export
-dBP <- function(x,mu=1,sigma=1,log=FALSE)
-{
-  if (any(mu < 0))
-    stop(paste("mu must be positive", "\n", ""))
-  if (any(sigma < 0))
-    stop(paste("sigma must be positive", "\n", ""))
-  if (any(x <= 0))
-    stop(paste("x must be positive", "\n", ""))
-
-  a <- mu*(1+sigma)
-  b <- 2 + sigma
-
-  fy <- dbetapr(x, shape1 = a, shape2 = b, scale = 1, log = log)
-  fy
-
-}
-#' Cumulative Distribution Function
-#'
-#'
-#'
-#'@export
-
-pBP <-  function(q,mu=1,sigma=1, lower.tail = TRUE, log.p = FALSE)
-{
-  if (any(mu < 0))
-    stop(paste("mu must be positive", "\n", ""))
-  if (any(sigma < 0))
-    stop(paste("sigma must be positive", "\n", ""))
-  if (any(q < 0))
-    stop(paste("q must be positive", "\n", ""))
-
-  a <- mu*(1+sigma)
-  b <- 2 + sigma
-
-  cdf <- pbetapr(q, shape1 = a, shape2 = b, scale=1, lower.tail = lower.tail,
-                 log.p = log.p)
-  cdf
-}
-
-#' Random Numbers
-#'
-#'
-#'
-#'@export
-
-rBP <- function(n,mu=1,sigma=1)
-{
-  if (any(mu < 0))
-    stop(paste("mu must be positive", "\n", ""))
-  if (any(sigma < 0))
-    stop(paste("sigma must be positive", "\n", ""))
-  if (any(n <= 0))
-    stop(paste("n must be a positive integer", "\n", ""))
-
-  n <- ceiling(n)
-
-  a <- mu*(1+sigma)
-  b <- 2 + sigma
-
-  r <- rbetapr(n,shape1=a,shape2=b,scale=1)
-
-  r
-}
-
-
-#' Quantile Function
-#'
-#'
-#'
-#'@export
-
-
-qBP <- function(p, mu = 1, sigma = 1, lower.tail = TRUE, log.p = FALSE)
-{
-  if (any(mu < 0))
-    stop(paste("mu must be positive", "\n", ""))
-  if (any(sigma < 0))
-    stop(paste("sigma must be positive", "\n", ""))
-  if (any(p <= 0) | any(p >= 1))
-    stop(paste("p must be between 0 and 1", "\n", ""))
-
-  a <- mu*(1+sigma)
-  b <- 2 + sigma
-
-  q <- qbetapr(p, shape1 = a, shape2 = b,scale=1, lower.tail = lower.tail, log.p = log.p)
-
-  q
-}
-
-
-
-#' gamlss BP
-#'
-#'
-#'
-#'@export
-BP <- function (mu.link = "log", sigma.link = "log")
-{
-  mstats <- checklink("mu.link", "Beta Prime", substitute(mu.link), c("log", "identity", "sqrt"))
-  dstats <- checklink("sigma.link", "Beta Prime",substitute(sigma.link), c("log", "identity", "sqrt"))
-  structure(list(family = c("BP", "Beta Prime"),
-                 parameters = list(mu = TRUE,sigma = TRUE), nopar = 2, type = "Continuous",
-                 mu.link = as.character(substitute(mu.link)),
-                 sigma.link = as.character(substitute(sigma.link)),
-                 mu.linkfun = mstats$linkfun,
-                 sigma.linkfun = dstats$linkfun,
-                 mu.linkinv = mstats$linkinv,
-                 sigma.linkinv = dstats$linkinv,
-                 mu.dr = mstats$mu.eta,
-                 sigma.dr = dstats$mu.eta,
-
-
-                 dldm = function(y, mu, sigma){
-                   a <- mu*(1+sigma)
-                   b <- mu*(1+sigma)+sigma+2
-                   Phi <-  (1+sigma)
-                   yast <- log(y) - log(1+y)
-                   muast <- digamma(a) - digamma(b)
-                   dldm <- Phi*(yast - muast)
-
-                   dldm
-                 },
-                 d2ldm2 = function(mu, sigma){
-                   Phi2 <- (1+sigma)^2
-                   a <- mu*(1+sigma)
-                   b <- mu*(1+sigma)+sigma+2
-                   d2dldm2 <- -Phi2*(trigamma(a) - trigamma(b))
-
-                   d2dldm2
-                 },
-                 dldd = function(y, mu, sigma){
-                   Phi <-  (1+sigma)
-                   a <- mu*(1+sigma)
-                   b <- mu*(1+sigma)+sigma+2
-                   ystar <- mu*log(y) - (1+mu)*log(1+y)
-                   mustar <- mu*digamma(a) - (1+mu)*digamma(b) + digamma(Phi+1)
-
-                   dldd <- ystar - mustar
-
-                   dldd
-                 },
-                 d2ldd2 = function(mu,sigma){
-                   Phi <-  (1+sigma)
-                   a <- mu*(1+sigma)
-                   b <- mu*(1+sigma)+sigma+2
-
-                   d2ldd2 <- -(mu^2)*trigamma(a) + ((1+mu)^2)*trigamma(b) - trigamma(Phi+1)
-
-                   d2ldd2
-
-                 },
-                 d2ldmdd = function(mu,sigma){
-
-                   a <- mu*(1+sigma)
-                   b <- mu*(1+sigma)+sigma+2
-                   Phi <-  (1+sigma)
-                   gammaast <- Phi*(trigamma(b) + mu*(trigamma(b)-trigamma(a)))
-
-                   d2ldmdd <- gammaast
-
-                   d2ldmdd
-
-                 },
-                 G.dev.incr = function(y, mu, sigma,...){-2*dBP(y, mu, sigma, log = TRUE)},
-                 rqres = expression(rqres(pfun = "pBP", type = "Continuous", y = y, mu = mu, sigma = sigma)),
-                 mu.initial = expression({mu <- mean(y)}),
-                 sigma.initial = expression({sigma <-  mean(y)*(1+mean(y))/var(y) }),
-                 mu.valid = function(mu) all(mu > 0),
-                 sigma.valid = function(sigma) all(sigma > 0),
-                 y.valid = function(y) all(y > 0)),
-            class = c("gamlss.family","family"))
-}
-
-
 #' GEE BP
 #'
 #'
 #'
 #'@export
-geeBP = function(formula, data, id, tol = 0.001, maxiter = 25, corstr = "independence", linkmu = "log", print=FALSE){
+geeBP = function(formula, data, id, tol = 0.001, maxiter = 1000, corstr = "independence", linkmu = "log", print=FALSE){
   
   namescor = c("independence", "unstructured", "exchangeable", "AR-1", "one-dependent",
                "one-dependent-stat","two-dependent","two-dependent-stat")
@@ -216,7 +37,7 @@ geeBP = function(formula, data, id, tol = 0.001, maxiter = 25, corstr = "indepen
   }
   
   beta = mod0$mu.coefficients # Chute inicial para beta
-  phi = max(0.01,mod0$sigma.coefficients) # Chute inicial para phi
+  phi = max(0.5,mod0$sigma.coefficients) # Chute inicial para phi
   
   # Modelo sob suposição de dependência
   cont = 1
@@ -255,18 +76,27 @@ geeBP = function(formula, data, id, tol = 0.001, maxiter = 25, corstr = "indepen
     uc = split(u,id)
     scomb = matrix(u,n,nr[1],byrow = TRUE)
     if(corstr == "unstructured"){
+      
       R = matrix(0,nr[1],nr[1])
-      for(j in 1:nr[1]){
-        for(l in j:nr[1]){
-          num = sum(scomb[,j]*scomb[,l])
-          den1 = sqrt(sum(scomb[,j]^2))
-          den2 = sqrt(sum(scomb[,l]^2))
-          R[j,l] = num/(den1*den2)
-          R[l,j] = R[j,l]
+      diag(R) <- 1
+      a <- sapply(1:nr[1] ,function(j){
+        l <- j:nr[1]
+        scomb = matrix(1:(n*nr),n,nr,byrow = TRUE)
+        out <- sapply(l,function(l){
+          num <- sum(scomb[,j]*scomb[,l])
+          den1 <- sqrt(sum(scomb[,j]^2))
+          den2 <- sqrt(sum(scomb[,l]^2))
+          r <- num/(den1*den2)
         }
+        )
       }
-      diag(R) = 1
-      Rm = kronecker(diag(n),R)
+      ) %>% unlist 
+      R[upper.tri(R, diag = FALSE)] <- a[a < 0.99999999]
+      R <- Matrix::forceSymmetric(R)
+      print(R)
+      Rm <- kronecker(diag(n),R)
+      
+      
     } else if(corstr == "AR-1"){
       cnum = cden1 = cden2 = 0
       for(i in 1:n){
@@ -358,6 +188,7 @@ geeBP = function(formula, data, id, tol = 0.001, maxiter = 25, corstr = "indepen
         }
         alpha1[j] = num/den
       }
+      
       alpha1 = (N/n)*alpha1
       alpha2 = 0
       for(j in 1:(nr[1]-2)){
@@ -469,7 +300,7 @@ geeBP = function(formula, data, id, tol = 0.001, maxiter = 25, corstr = "indepen
     
     # Verificar se convergiu: beta1 é aproximadamente beta
     dif = abs(beta1-beta)
-    if(sum(dif)<=(tol*p)){
+    if(sum(dif) <= (tol*p)){
       beta = beta1
       if(print == TRUE) cat("The algorithm converged")
       converg = 1
